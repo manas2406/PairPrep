ee4ada2ab02bca2c71202e22d8bcf46ef64c0c6f
manas2406 <manassh2406@gmail.com>
2026-04-02 11:14:44 +0530

    various bug fixes

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,6 +3,12 @@ const nextConfig = {
   /* config options here */
   reactCompiler: true,
   reactStrictMode: false,
+  experimental: {
+    allowedDevOrigins: ['172.70.96.220', 'localhost', '127.0.0.1'],
+  },
+  turbopack: {
+    root: '.',
+  },
 };
 
 export default nextConfig;
